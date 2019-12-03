4e3cdb03123bf157f4d42e551b7904e8cc3a4a91
Aviv Issachar <Avivissachar73@gmail.com>
2019-12-03 12:51:53 +0200

    ...

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const cors = require('cors');
 
 const app = express();
 
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 app.use(cookieParser());

5b4bb69144f66c1a070e6abb6d74bc90c3763365
Aviv Issachar <Avivissachar73@gmail.com>
2019-12-03 12:49:16 +0200

    fixing auto login bug

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const cors = require('cors');
 
 const app = express();
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(bodyParser.json());

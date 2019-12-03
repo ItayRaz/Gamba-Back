ec5e2e9f3f3385795ba7afe0744d6975a4292845
Aviv Issachar <Avivissachar73@gmail.com>
2019-12-03 17:33:35 +0200

    fixing credentionals bug

--- server.js
+++ server.js
@@ -2,40 +2,44 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const cors = require('cors');
 
 const app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(bodyParser.json());
+
 app.use(session({
-    secret: 'some secret sheet',
+    secret: 'some secret shit',
     resave: false,
     saveUninitialized: true,
     cookie: {secure: false}
 }));
-app.use(cors());
 
+const corsOptions = {
+    origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
+    credentials: true
+};
+app.use(cors(corsOptions));
 
-const connectToSockets = require('./api/socket/socket.rout.js')
 
 const eventoRout = require('./api/evento/evento.rout.js');
 const userRout = require('./api/user/user.rout.js');
 const reviewRout = require('./api/review/review.rout.js');
+const connectToSockets = require('./api/socket/socket.rout.js')
 
 app.use('/api/evento', eventoRout);
 app.use('/api/user', userRout);
 app.use('/api/review', reviewRout);
-
-
 connectToSockets(io);
+
+
 
 // module.exports = app;
 module.exports = http;

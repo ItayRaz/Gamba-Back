2d81732bea03ddd34d27558dbb7e04ef0b454507
Paz Gerti <pazgerti4@gmail.com>
2019-12-04 10:08:32 +0200

    test8

--- services/db.service.js
+++ services/db.service.js
@@ -31,8 +31,8 @@ async function getCollection(collectionName) {
 async function connect() {
     if (dbConn) return dbConn;
     try {
-        // const client = await MongoClient.connect(dbURL, {useNewUrlParser: true});
-        const client = await new MongoClient(dbURL, {useNewUrlParser: true});
+        const client = await MongoClient.connect(dbURL, {useNewUrlParser: true});
+        // const client = await new MongoClient(dbURL, {useNewUrlParser: true});
         const db = client.db(dbName);
         dbConn = db;
         return db;
